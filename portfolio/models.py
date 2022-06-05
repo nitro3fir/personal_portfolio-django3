2c4b6f45f377ade1c778302d63194f4de378896b
Клим <klim2003115@gmail.com>
2022-06-05 13:35:20 +0000

    Added scripts

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,3 +8,13 @@ class Project(models.Model):
     
     def __str__(self):
         return self.title
+
+class Script(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images')
+    file = models.FileField(upload_to='portfolio/scripts', blank=True)
+    url = models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.title
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Script
 
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects': projects})
+    scripts = Script.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects, 'scripts': scripts})
 
 def detail(request, blog_id):
     return render(request, 'blog/detail.html', {'id': blog_id})
